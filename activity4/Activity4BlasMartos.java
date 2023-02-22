a1d0cba3d5390014bf9744a1476a7c9503a9668e
BlasMartos <04blasm@inscastellet.cat>
2023-02-22 16:38:28 +0100

    Modificated the file

--- activity4/Activity4BlasMartos.java
+++ activity4/Activity4BlasMartos.java
@@ -3,8 +3,12 @@ package activity4;
 public class Activity4BlasMartos {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-
+		System.out.println("Tabla de multiplicar del 2: ");
+		final int TWO =2;
+		for (int i = 1; i <=10;i++) {
+			System.out.println( TWO + " * " + i + " = " + (TWO*i) );
+			
+		}
 	}
 
 }
